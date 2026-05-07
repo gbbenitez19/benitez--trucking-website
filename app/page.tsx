90f822d0367e6c2c4f4d736fe98422f595526844
Garry B. Benitez <garry.benitez019@gmail.com>
2026-05-07 22:04:48 +0800

    adjust hero image size

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function TruckingBusinessWebsite() {
             <img
               src="/BTS_Company Logo.png"
               alt="BTS Logo"
-              className="w-30 h-30 object-cover"
+              className="w-50 h-50 object-cover"
             />
 
             <div>
